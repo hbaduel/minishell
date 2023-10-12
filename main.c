77f41ac61dd94ff697b291f15ad0eb57bda5439c
hbaduel <hugo.baduel81@gmail.com>
2023-10-12 14:35:26 +0200

    correction redir

--- main.c
+++ main.c
@@ -49,25 +49,30 @@ void	ft_chooseaction(t_data *data)
 				redir_input("test", data);
 			i++;
 		}
-		/*if (terminal[i] == '>')
+		if (terminal[i] == '>')
 		{
 			if (terminal[i + 1] == '>')
 			{
-				redir_append;
+				redir_append("res", data);
 				i++;
 			}
 			else
-				redir_outputput();
+				redir_output("res", data);
 			i++;
-		}*/
+		}
 		i++;
 	}
-	if (data->infilefd != -1)
+	if (data->infilefd != 0)
 		close(data->infilefd);
+	if (data->outfilefd != 1)
+		close(data->outfilefd);
+	exit(0);
 }
 
 void	ft_readterminal(t_data *data)
 {
+	pid_t	cpid;
+
 	while (1)
 	{
 		terminal = readline("\e[1;35mmi\e[1;34mni\e[1;32msh\e[1;33mel\e[1;31ml>\e[0;37m ");
@@ -76,7 +81,12 @@ void	ft_readterminal(t_data *data)
 			free(terminal);
 			return ;
 		}
-		ft_chooseaction(data);
+		cpid = fork();
+		if (cpid == -1)
+			ft_exiterror("fork");
+		if (cpid == 0)
+			ft_chooseaction(data);
+		waitpid(cpid, NULL, 0);
 		add_history(terminal);
 		free(terminal);
 	}
@@ -87,7 +97,7 @@ int	main(int argc, char **argv, char **envp)
 	t_data	*data;
 
 	data = malloc(sizeof(t_data));
-	data->infilefd = -1;
+	data->infilefd = 0;
 	ft_readterminal(data);
 	rl_clear_history();
 	return (0);
--- redir.c
+++ redir.c
@@ -2,22 +2,24 @@
 
 void	redir_input(char *file, t_data *data)
 {
-	char	*test;
-
 	data->infilefd = open(file, O_RDONLY, 0777);
 	if (data->infilefd < 0)
 		ft_exiterror("open");
 	dup2(data->infilefd, 0);
-	test = ft_readfd(0);
-	ft_putstr_fd(test, 1);
 }
 
 void	redir_output(char *file, t_data *data)
 {
-
+	data->outfilefd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (data->outfilefd < 0)
+		ft_exiterror("open");
+	dup2(data->outfilefd, 1);
 }
 
 void	redir_append(char *file, t_data *data)
 {
-
+	data->outfilefd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0777);
+	if (data->outfilefd < 0)
+		ft_exiterror("open");
+	dup2(data->outfilefd, 1);
 }
